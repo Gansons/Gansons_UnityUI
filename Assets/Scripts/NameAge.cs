7f48243ee9fe892a205a30607ef58b3df0225618
Gansons <gansonsraivis@gmail.com>
2026-02-26 01:14:44 +0200

    Skaņas effekti

---
+++ Assets/Scripts/NameAge.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+
+public class NameAge : MonoBehaviour
+{
+    public TMP_InputField nameInput;
+    public TMP_InputField yearInput;
+    public TMP_Text outputText;
+
+    public void ShowResult()
+    {
+        string name = nameInput.text;
+        string yearText = yearInput.text;
+
+        // Vārda pārbaude
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            outputText.text = "Ievadi vārdu!";
+            return;
+        }
+
+        // Gadam jābūt 4 cipariem
+        if (yearText.Length != 4)
+        {
+            outputText.text = "Ievadi 4 ciparu dzimšanas gadu!";
+            return;
+        }
+
+        // Pārbauda vai ir skaitlis
+        if (!int.TryParse(yearText, out int birthYear))
+        {
+            outputText.text = "Nederīgs gads!";
+            return;
+        }
+
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+        // Drošības pārbaude
+        if (age < 0 || age > 120)
+        {
+            outputText.text = "Nederīgs vecums!";
+            return;
+        }
+
+        // Gala teksts
+        outputText.text = $"{name}, {age} gadus vecs ir varonis.";
+    }
+}
